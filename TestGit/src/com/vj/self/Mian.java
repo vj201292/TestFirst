6a8418e90668432ba2f6e6176831eb2e3771672e
vijay201292 <vijay.ku.99@gmail.com>
2017-07-31 09:59:28 +0530

    First change add a simple output

--- TestGit/src/com/vj/self/Mian.java
+++ TestGit/src/com/vj/self/Mian.java
@@ -10,6 +10,7 @@ public class Mian {
 	public static void displayGretting(){
 		System.out.println("Hi! Guys");
 		System.out.println(" This is my first test git proj.");
+		System.out.println("Test Test");
 	}
 
 }
