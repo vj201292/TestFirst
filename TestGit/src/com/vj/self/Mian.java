e4937fcfb85a6386a3d2adb86a1dc7a1600cb863
vijay201292 <vijay.ku.99@gmail.com>
2017-07-31 09:46:44 +0530

    initial commit..

---
+++ TestGit/src/com/vj/self/Mian.java
@@ -0,0 +1,15 @@
+package com.vj.self;
+
+public class Mian {
+
+	public static void main(String[] args) {
+		displayGretting();
+
+	}
+	
+	public static void displayGretting(){
+		System.out.println("Hi! Guys");
+		System.out.println(" This is my first test git proj.");
+	}
+
+}
